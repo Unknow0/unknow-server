4fb19366adb749d10599d9ca8bde0b86d2c383bb
Unknow0 <unknow0@free.fr>
2021-10-30 16:30:32 +0200

    update bensh script

--- .github/bench.sh
+++ .github/bench.sh
@@ -1,27 +1,57 @@
 #!/bin/bash
 
+TESTS=(
+	'missing'
+	'test'
+	'Webservice POST <.github/bare.xml'
+	)
+SERVER=(unknow tomcat)
+
 run() {
-	siege -b -c 25 -t 60s http://127.0.0.1:8080/$1 2>/dev/null | jq .
+	[[ '$1' = 'Webservice POST <.github/bare.xml' ]] && return
+	siege --no-parser -b -c 2 -t10s "http://win7:8080/$1" 2>/dev/null | jq .transaction_rate
 }
 
 dotests() {
-	run 'missing'
-	run 'test'
+	for t in ${TESTS[@]}; do run "$t"; done
 }
 
-unknow_init() {
+unknow_start() {
 	java -jar unknow-http-test/target/server.jar >/dev/null 2>/dev/null &
 	pid=$!
 	trap "kill -9 $pid" EXIT
 }
+unknow_stop() {
+	kill -9 $pid
+}
+tomcat_start() {
+	cp unknow-http-test/target/*.war /usr/share/tomcat9/webapps/ROOT.war
+	/usr/share/tomcat9/bin/catalina.sh run >/dev/null 2>/dev/null &
+	pid=$!
+	trap "kill -9 $pid" EXIT
+}
+tomcat_stop() {
+	/usr/share/tomcat9/bin/shutdown.sh
+}
 
 declare -a results
-for i in unknow
+for i in ${SERVER[@]}
 do
-	$i_init
+	$i_start
 	echo "warmup $i"
 	dotests > /dev/null
+	echo "testing $i"
 	results+=("$(dotests)")
+	$i_stop
 done
 
-declare -p results;
+printf '%-10s' 'server'
+for i in ${TEST[@]}; do printf '%-10s' "${i## *}"; done
+echo
+IFS=$'\n'
+for((i=0; i<${#SERVER[@]}; i++))
+do
+	printf '%-10s' "${SERVER[$i]}"
+	for r in ${results[$i]}; do printf '%-10s' "$r"; done
+	echo
+done
