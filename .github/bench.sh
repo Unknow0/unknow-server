a496699e1d143a9c34baaccbb449e23d9c9e4124
Unknow0 <unknow0@free.fr>
2021-10-30 16:54:22 +0200

    add log in bench

--- .github/bench.sh
+++ .github/bench.sh
@@ -9,9 +9,7 @@ SERVER=(unknow tomcat)
 
 run() {
 	[[ '$1' = 'Webservice POST <.github/bare.xml' ]] && return
-	r=$(siege --no-parser -b -c 2 -t10s "http://win7:8080/$1" 2>/dev/null)
-	echo "$r" >&2
-	echo "$r" | jq .transaction_rate
+	siege -v --no-parser -b -c 2 -t10s "http://127.0.0.1:8080/$1" 2>/dev/null | jq .transaction_rate
 }
 
 dotests() {
