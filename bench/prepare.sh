44daffa59c136e3b0945e5f457ab015b09e3c0f7
Unknow0 <unknow0@free.fr>
2022-04-03 17:08:14 +0200

    move bench init

---
+++ bench/prepare.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# install dependencies
+VER=$(wget -q -O - https://dlcdn.apache.org/tomcat/tomcat-9/ | grep '<a href="v9' | sed 's:.*>v\(9[^</]*\)/</a.*:\1:' | sort -rV | head -1)
+mkdir -p tomcat
+wget -O - https://dlcdn.apache.org/tomcat/tomcat-9/v$VER/bin/apache-tomcat-$VER.tar.gz | tar xz --strip-components=1 -C tomcat
+VER=$(wget -q -O - https://archive.apache.org/dist/jmeter/binaries/ | grep -Po '(?<=>apache-jmeter-).*(?=.tgz)' | sort -rV | head -1)
+mkdir -p jmeter
+wget -O -  https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-$VER.tgz | tar xz --strip-components=1 -C jmeter
+
+# avoid goiing out of ephemeral port
+sysctl net.ipv4.tcp_tw_reuse=1
