84459e55d257b4769a81958b69697ef7d3ed887d
Unknow0 <unknow0@free.fr>
2022-07-19 00:19:30 +0200

    test native-image

--- bench/run.sh
+++ bench/run.sh
@@ -10,6 +10,16 @@ unknow_stop() {
 	pid=
 	sleep 2
 }
+native_start() {
+	./server-native >log 2>log &
+	pid=$!
+	sleep 5
+}
+native_stop() {
+	kill -9 $pid
+	pid=
+	sleep 2
+}
 tomcat_start() {
 	cp unknow-http-test/target/*.war $CATALINA_HOME/webapps/ROOT.war || exit 1
 	$CATALINA_HOME/bin/catalina.sh run >/dev/null 2>/dev/null || exit 1 &
@@ -27,7 +37,7 @@ tomcat_stop() {
 mkdir out
 trap '[[ "$pid" ]] && kill -9 $pid' EXIT
 
-for i in tomcat unknow
+for i in tomcat unknow native
 do
 	${i}_start
 	$JMETER -n -t bench/test.jmx -Jhost=127.0.0.1 -Jport=8080 -Jout=out/$i.csv
