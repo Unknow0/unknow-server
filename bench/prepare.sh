0c468b626ea0836f103b2e319e6996ff29c592c7
Unknow0 <unknow0@free.fr>
2022-08-19 16:08:21 +0200

    update bench to use tomcat10

--- bench/prepare.sh
+++ bench/prepare.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+set -e
+
+TOMCAT=10
+
 # install dependencies
-VER=$(wget -q -O - https://dlcdn.apache.org/tomcat/tomcat-10/ | grep '<a href="v9' | sed 's:.*>v\(10[^</]*\)/</a.*:\1:' | sort -rV | head -1)
+VER=$(wget -q -O - "https://dlcdn.apache.org/tomcat/tomcat-$TOMCAT/" | grep "<a href=\"v$TOMCAT." | sed "s:.*>v\($TOMCAT[^</]*\)/</a.*:\1:" | sort -rV | head -1)
 mkdir -p tomcat
-wget -O - https://dlcdn.apache.org/tomcat/tomcat-10/v$VER/bin/apache-tomcat-$VER.tar.gz | tar xz --strip-components=1 -C tomcat
+wget -O - https://dlcdn.apache.org/tomcat/tomcat-$TOMCAT/v$VER/bin/apache-tomcat-$VER.tar.gz | tar xz --strip-components=1 -C tomcat
 rm -r tomcat/webapps/*
 VER=$(wget -q -O - https://archive.apache.org/dist/jmeter/binaries/ | grep -Po '(?<=>apache-jmeter-).*(?=.tgz)' | sort -rV | head -1)
 mkdir -p jmeter
