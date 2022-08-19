5d0d462a93190d425b4c9b297cc8c68615300ff4
Unknow0 <unknow0@free.fr>
2022-08-19 15:40:40 +0200

    move javax.xml.bin to jakarta

--- bench/prepare.sh
+++ bench/prepare.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # install dependencies
-VER=$(wget -q -O - https://dlcdn.apache.org/tomcat/tomcat-9/ | grep '<a href="v9' | sed 's:.*>v\(9[^</]*\)/</a.*:\1:' | sort -rV | head -1)
+VER=$(wget -q -O - https://dlcdn.apache.org/tomcat/tomcat-10/ | grep '<a href="v9' | sed 's:.*>v\(10[^</]*\)/</a.*:\1:' | sort -rV | head -1)
 mkdir -p tomcat
-wget -O - https://dlcdn.apache.org/tomcat/tomcat-9/v$VER/bin/apache-tomcat-$VER.tar.gz | tar xz --strip-components=1 -C tomcat
+wget -O - https://dlcdn.apache.org/tomcat/tomcat-10/v$VER/bin/apache-tomcat-$VER.tar.gz | tar xz --strip-components=1 -C tomcat
 rm -r tomcat/webapps/*
 VER=$(wget -q -O - https://archive.apache.org/dist/jmeter/binaries/ | grep -Po '(?<=>apache-jmeter-).*(?=.tgz)' | sort -rV | head -1)
 mkdir -p jmeter
