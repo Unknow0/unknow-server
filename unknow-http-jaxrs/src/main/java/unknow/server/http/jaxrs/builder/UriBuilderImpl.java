944638bf65108cdac649146ccf3a92a0eee4846f
Unknow0 <unknow0@free.fr>
2023-05-06 15:12:43 +0200

    9 finish uri builder

--- unknow-http-jaxrs/src/main/java/unknow/server/http/jaxrs/builder/UriBuilderImpl.java
+++ unknow-http-jaxrs/src/main/java/unknow/server/http/jaxrs/builder/UriBuilderImpl.java
@@ -8,9 +8,15 @@ import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 import jakarta.ws.rs.core.MultivaluedHashMap;
 import jakarta.ws.rs.core.MultivaluedMap;
@@ -19,6 +25,28 @@ import jakarta.ws.rs.core.UriBuilderException;
 import unknow.server.http.jaxrs.JaxrsReq;
 
 public class UriBuilderImpl extends UriBuilder {
+	private static final BitSet dontNeedEncoding;
+	static {
+		dontNeedEncoding = new BitSet(256);
+		int i;
+		for (i = 'a'; i <= 'z'; i++)
+			dontNeedEncoding.set(i);
+		for (i = 'A'; i <= 'Z'; i++)
+			dontNeedEncoding.set(i);
+		for (i = '0'; i <= '9'; i++)
+			dontNeedEncoding.set(i);
+		dontNeedEncoding.set(' ');
+		dontNeedEncoding.set('-');
+		dontNeedEncoding.set('_');
+		dontNeedEncoding.set('.');
+		dontNeedEncoding.set('*');
+		dontNeedEncoding.set('/');
+	}
+	private static final char[] digits = {
+			'0', '1', '2', '3', '4', '5',
+			'6', '7', '8', '9', 'a', 'b',
+			'c', 'd', 'e', 'f' };
+
 	private String scheme;
 	private String ssp;
 	private String userInfo;
@@ -204,11 +232,7 @@ public class UriBuilderImpl extends UriBuilder {
 
 	@Override
 	public UriBuilder resolveTemplates(Map<String, Object> templateValues) {
-		if (templateValues.isEmpty())
-			return this;
-
-		// TODO Auto-generated method stub
-		return this;
+		return resolveTemplates(templateValues, true);
 	}
 
 	@Override
@@ -216,8 +240,11 @@ public class UriBuilderImpl extends UriBuilder {
 		if (templateValues.isEmpty())
 			return this;
 
-		// TODO Auto-generated method stub
-		return this;
+		Function<String, String> f = n -> {
+			Object o = templateValues.get(n);
+			return o == null ? null : o.toString();
+		};
+		return resolve((v, path) -> encode(replace(v, f), encodeSlashInPath || !path));
 	}
 
 	@Override
@@ -225,13 +252,17 @@ public class UriBuilderImpl extends UriBuilder {
 		if (templateValues.isEmpty())
 			return this;
 
-		// TODO Auto-generated method stub
-		return this;
+		Function<String, String> f = n -> {
+			Object o = templateValues.get(n);
+			return o == null ? null : o.toString();
+		};
+		return resolve((v, path) -> encodePercent(replace(v, f)));
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public URI buildFromMap(Map<String, ?> values) {
-		return buildFromMap(values, true);
+		return URI.create(clone().resolveTemplates((Map<String, Object>) values, true).toTemplate());
 	}
 
 	@SuppressWarnings("unchecked")
@@ -253,14 +284,36 @@ public class UriBuilderImpl extends UriBuilder {
 
 	@Override
 	public URI build(Object[] values, boolean encodeSlashInPath) throws IllegalArgumentException, UriBuilderException {
-		// TODO Auto-generated method stub
-		return null;
+		if (values.length == 0)
+			return URI.create(toTemplate());
+
+		List<String> list = new ArrayList<>();
+		Function<String, String> f = n -> {
+			int i = list.indexOf(n);
+			if (i < 0) {
+				i = list.size();
+				list.add(n);
+			}
+			return i >= values.length ? null : String.valueOf(values[i]);
+		};
+		return URI.create(clone().resolve((v, path) -> encode(replace(v, f), encodeSlashInPath || !path)).toTemplate());
 	}
 
 	@Override
 	public URI buildFromEncoded(Object... values) throws IllegalArgumentException, UriBuilderException {
-		// TODO Auto-generated method stub
-		return null;
+		if (values.length == 0)
+			return URI.create(toTemplate());
+
+		List<String> list = new ArrayList<>();
+		Function<String, String> f = n -> {
+			int i = list.indexOf(n);
+			if (i < 0) {
+				i = list.size();
+				list.add(n);
+			}
+			return i >= values.length ? null : String.valueOf(values[i]);
+		};
+		return URI.create(clone().resolve((v, path) -> encodePercent(replace(v, f))).toTemplate());
 	}
 
 	@Override
@@ -301,7 +354,7 @@ public class UriBuilderImpl extends UriBuilder {
 	}
 
 	@Override
-	public UriBuilder clone() {
+	public UriBuilderImpl clone() {
 		UriBuilderImpl uri = new UriBuilderImpl();
 		uri.scheme = scheme;
 		uri.ssp = ssp;
@@ -313,6 +366,104 @@ public class UriBuilderImpl extends UriBuilder {
 		uri.query.putAll(query);
 		uri.fragment = fragment;
 		return uri;
+	}
+
+	private UriBuilderImpl resolve(BiFunction<String, Boolean, String> resolveEncode) {
+		ListIterator<PathPart> it = paths.listIterator();
+		while (it.hasNext()) {
+			PathPart p = it.next();
+			String s = resolveEncode.apply(p.path, false);
+			if (!s.equals(p.path)) {
+				p = new PathPart(s, p.matrix);
+				it.set(p);
+			}
+
+			MultivaluedMap<String, String> m = null;
+			Iterator<Entry<String, List<String>>> itk = p.matrix.entrySet().iterator();
+			while (itk.hasNext()) {
+				Entry<String, List<String>> next = itk.next();
+				String replace = resolveEncode.apply(next.getKey(), true);
+				if (!replace.equals(next.getKey())) {
+					if (m == null)
+						m = new MultivaluedHashMap<>();
+					m.put(replace, next.getValue());
+					itk.remove();
+				}
+
+				ListIterator<String> itv = next.getValue().listIterator();
+				while (itv.hasNext())
+					itv.set(resolveEncode.apply(itv.next(), true));
+			}
+
+			if (m != null)
+				p.matrix.putAll(m);
+		}
+		return this;
+	}
+
+	private static boolean isHex(char c) {
+		return c >= '0' && c <= '9' || c >= 'a' && c <= 'f' || c >= 'A' && c <= 'F';
+	}
+
+	/**
+	 * encode all
+	 * 
+	 * @param value the value to encode
+	 * @return
+	 */
+	private static String encode(String value, boolean encodeSlash) {
+		StringBuilder sb = new StringBuilder();
+		int l = 0;
+		for (int i = 0; i < value.length(); i++) {
+			int c = value.charAt(i);
+			if (!dontNeedEncoding.get(c) || c == '/' && encodeSlash) {
+				sb.append(value.substring(l, i));
+				if (c > 256)
+					sb.append('%').append(digits[(c >> 12) & 0xF]).append(digits[(c >> 8) & 0xF]);
+				sb.append('%').append(digits[(c >> 4) & 0xF]).append(digits[c & 0xF]);
+				l = i + 1;
+			}
+		}
+		return l == 0 ? value : sb.append(value.substring(l)).toString();
+	}
+
+	/**
+	 * encode only % not followed by hex char
+	 * 
+	 * @param value the value to encode
+	 * @return
+	 */
+	private static String encodePercent(String value) {
+		StringBuilder sb = new StringBuilder();
+
+		int l = 0;
+		for (int i = 0; i < value.length(); i++) {
+			int c = value.charAt(i);
+			if (c == '%' && i < value.length() - 2 && !isHex(value.charAt(i + 1)) || !isHex(value.charAt(i + 2))) {
+				sb.append(value.substring(l, i)).append("%25");
+				l = i + 1;
+			}
+		}
+		return l == 0 ? value : sb.append(value.substring(l)).toString();
+	}
+
+	private static String replace(String value, Function<String, String> r) {
+		int i = value.indexOf('{');
+		if (i < 0)
+			return value;
+		i++;
+		int l = value.indexOf('}', i);
+		if (l < 0)
+			return value;
+		int c = value.indexOf(':', i);
+		if (c < l && c > 0)
+			c = l;
+		String s = r.apply(value.substring(i, c).trim());
+		if (s == null)
+			return value;
+		if (i > 0)
+			s = value.subSequence(0, i - 1) + s;
+		return l == value.length() - 1 ? s : s + value.substring(l + 1);
 	}
 
 	private static final class PathPart {
