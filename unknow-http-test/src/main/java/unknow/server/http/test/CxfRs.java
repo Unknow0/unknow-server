e1cd22d9893aa6359ada392945daf98f4be275bb
Unknow0 <unknow0@free.fr>
2023-08-13 12:53:45 +0200

    9 remove accessLog by default and fic cxfRs

--- unknow-http-test/src/main/java/unknow/server/http/test/CxfRs.java
+++ unknow-http-test/src/main/java/unknow/server/http/test/CxfRs.java
@@ -12,17 +12,23 @@ import org.apache.cxf.feature.Feature;
 import org.apache.cxf.jaxrs.openapi.OpenApiFeature;
 import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
 
+import com.fasterxml.jackson.jakarta.rs.json.JacksonJsonProvider;
+
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 
 /**
  * @author unknow
  */
-@WebServlet(value = "/cxf-rs/*", initParams = @WebInitParam(name = "jaxrs.address", value = "/cxf-rs"))
+@WebServlet(value = "/cxf-rs/*")
 public class CxfRs extends CXFNonSpringJaxrsServlet {
 	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected List<?> getProviders(ServletConfig servletConfig, String splitChar) throws ServletException {
+		return Arrays.asList(new JacksonJsonProvider());
+	}
 
 	@Override
 	protected List<? extends Feature> getFeatures(ServletConfig servletConfig, String splitChar) throws ServletException {
