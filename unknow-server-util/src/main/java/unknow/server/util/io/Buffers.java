b7d96b59d62ba8711c70eaf5f88a2056e14bf56b
Unknow0 <unknow0@free.fr>
2023-11-25 12:14:33 +0100

    gh-24 remove chunk pool

--- unknow-server-util/src/main/java/unknow/server/util/io/Buffers.java
+++ unknow-server-util/src/main/java/unknow/server/util/io/Buffers.java
@@ -584,14 +584,15 @@ public class Buffers {
 		 * @return a chunk
 		 */
 		public static Chunk get() {
-			synchronized (mutex) {
-				if (idle == null)
-					return new Chunk();
-				Chunk c = idle;
-				idle = idle.next;
-				c.next = null;
-				return c;
-			}
+			return new Chunk();
+//			synchronized (mutex) {
+//				if (idle == null)
+//					return new Chunk();
+//				Chunk c = idle;
+//				idle = idle.next;
+//				c.next = null;
+//				return c;
+//			}
 		}
 
 		/**
@@ -602,11 +603,11 @@ public class Buffers {
 		 */
 		public static Chunk free(Chunk c) {
 			Chunk n = c.next;
-			c.o = c.l = 0;
-			synchronized (mutex) {
-				c.next = idle;
-				idle = c;
-			}
+//			c.o = c.l = 0;
+//			synchronized (mutex) {
+//				c.next = idle;
+//				idle = c;
+//			}
 			return n;
 		}
 	}
