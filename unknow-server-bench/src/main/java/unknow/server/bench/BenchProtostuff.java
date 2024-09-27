7f99fc8a751f3ef845ba360d828739e64d0eb5fc
Unknow <Unknow@win10>
2024-09-27 23:35:18 +0200

    add bench for protobuf

--- unknow-server-bench/src/main/java/unknow/server/bench/BenchProtostuff.java
+++ unknow-server-bench/src/main/java/unknow/server/bench/BenchProtostuff.java
@@ -3,8 +3,7 @@ package unknow.server.bench;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+import java.util.Arrays;
 
 import org.openjdk.jmh.annotations.Benchmark;
 
@@ -15,33 +14,33 @@ import io.protostuff.JsonIOUtil;
 import io.protostuff.JsonXIOUtil;
 import io.protostuff.LinkedBuffer;
 import io.protostuff.ProtostuffOutput;
-import io.protostuff.Schema;
-import io.protostuff.runtime.RuntimeSchema;
-import unknow.server.http.test.xml.Complex;
+import unknow.server.bench.protobuf.TestMsg.Builder;
+import unknow.server.bench.protostuff.TestMsg;
 
 public class BenchProtostuff {
-	private static final Complex TEST;
-
-	private static final Schema<Complex> SCHEMA = RuntimeSchema.createFrom(Complex.class);
-
 	private static final ObjectMapper m = new ObjectMapper();
 
+	private static final TestMsg TSTUFF;
+	private static final unknow.server.bench.protobuf.TestMsg TBUF;
 	static {
-		try (Reader r = new StringReader(BenchJaxb.XML)) {
-			TEST = (Complex) BenchJaxb.MOXY.createUnmarshaller().unmarshal(r);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
+		Builder n = unknow.server.bench.protobuf.TestMsg.newBuilder();
+		TestMsg t = new TestMsg();
+		for (int i = 0; i < 50; i++) {
+			t = new TestMsg().setLoopList(Arrays.asList(t.setTest("test" + i).setV(i)));
+			n = unknow.server.bench.protobuf.TestMsg.newBuilder().addLoop(n.setTest("test" + i).setV(i));
 		}
+		TSTUFF = t;
+		TBUF = n.build();
 	}
 
 	@Benchmark
 	public void jackson() throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		m.writeValue(out, TEST);
+		m.writeValue(out, TSTUFF);
 		out.flush();
 
 		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-		m.readValue(in, Complex.class);
+		m.readValue(in, TestMsg.class);
 	}
 
 	@Benchmark
@@ -49,22 +48,28 @@ public class BenchProtostuff {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 
 		LinkedBuffer buffer = LinkedBuffer.allocate(4096);
-		SCHEMA.writeTo(new ProtostuffOutput(buffer, out), TEST);
+		TSTUFF.cachedSchema().writeTo(new ProtostuffOutput(buffer, out), TSTUFF);
 		LinkedBuffer.writeTo(out, buffer);
 		out.flush();
 		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
-		Complex t = SCHEMA.newMessage();
-		SCHEMA.mergeFrom(new CodedInput(in, true), t);
+		TestMsg t = new TestMsg();
+		TSTUFF.cachedSchema().mergeFrom(new CodedInput(in, true), t);
 	}
 
 	@Benchmark
 	public void protostuffJson() throws IOException {
 
 		LinkedBuffer buffer = LinkedBuffer.allocate(4096);
-		byte[] byteArray = JsonXIOUtil.toByteArray(TEST, SCHEMA, false, buffer);
+		byte[] byteArray = JsonXIOUtil.toByteArray(TSTUFF, TSTUFF.cachedSchema(), false, buffer);
 
-		Complex t = SCHEMA.newMessage();
-		JsonIOUtil.mergeFrom(byteArray, t, SCHEMA, false);
+		TestMsg t = new TestMsg();
+		JsonIOUtil.mergeFrom(byteArray, t, TSTUFF.cachedSchema(), false);
 	}
 
+	@Benchmark
+	public void protobuf() throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		TBUF.writeTo(out);
+		TBUF.getParserForType().parseFrom(out.toByteArray());
+	}
 }
