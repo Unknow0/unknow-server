d16ec0ccd67705eebd78207fe5f1a36944bfe933
Simon <simon.antoine@koedia.com>
2024-09-27 16:02:05 +0200

    add bench on protostuff

---
+++ unknow-server-bench/src/main/java/unknow/server/bench/BenchProtostuff.java
@@ -0,0 +1,74 @@
+package unknow.server.bench;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.protostuff.CodedInput;
+import io.protostuff.JsonInput;
+import io.protostuff.JsonXOutput;
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtostuffOutput;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
+import unknow.server.http.test.xml.Complex;
+
+public class BenchProtostuff {
+	private static final Complex TEST;
+
+	private static final Schema<Complex> SCHEMA = RuntimeSchema.createFrom(Complex.class);
+
+	private static final ObjectMapper m = new ObjectMapper();
+
+	static {
+		try (Reader r = new StringReader(BenchJaxb.XML)) {
+			TEST = (Complex) BenchJaxb.MOXY.createUnmarshaller().unmarshal(r);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Benchmark
+	public void jackson() throws IOException {
+		PipedInputStream in = new PipedInputStream();
+		PipedOutputStream out = new PipedOutputStream(in);
+		m.writeValue(out, TEST);
+		out.flush();
+		m.readValue(in, Complex.class);
+	}
+
+	@Benchmark
+	public void protostuff() throws IOException {
+		PipedInputStream in = new PipedInputStream();
+		PipedOutputStream out = new PipedOutputStream(in);
+
+		LinkedBuffer buffer = LinkedBuffer.allocate(4096);
+		SCHEMA.writeTo(new ProtostuffOutput(buffer, out), TEST);
+		LinkedBuffer.writeTo(out, buffer);
+		out.flush();
+
+		Complex t = SCHEMA.newMessage();
+		SCHEMA.mergeFrom(new CodedInput(in, true), t);
+	}
+
+	@Benchmark
+	public void protostuffJson() throws IOException {
+		PipedInputStream in = new PipedInputStream();
+		PipedOutputStream out = new PipedOutputStream(in);
+
+		LinkedBuffer buffer = LinkedBuffer.allocate(4096);
+		SCHEMA.writeTo(new JsonXOutput(buffer, out, false, SCHEMA), TEST);
+		LinkedBuffer.writeTo(out, buffer);
+		out.flush();
+
+		Complex t = SCHEMA.newMessage();
+		SCHEMA.mergeFrom(new JsonInput(m.createParser(in), false), t);
+	}
+
+}
