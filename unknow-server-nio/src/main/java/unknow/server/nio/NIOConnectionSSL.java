12f656022cb6795bf7d31559e9863a640bcfd722
Unknow <Unknow@win10>
2024-09-30 23:06:58 +0200

    re add while

--- unknow-server-nio/src/main/java/unknow/server/nio/NIOConnectionSSL.java
+++ unknow-server-nio/src/main/java/unknow/server/nio/NIOConnectionSSL.java
@@ -67,22 +67,24 @@ public class NIOConnectionSSL extends NIOConnection {
 		if (processHandshake())
 			return;
 		int l;
-		l = channel.read(rawIn);
-		if (l == -1) {
-			in.close();
-			return;
-		}
-		if (l == 0 && rawIn.position() == 0)
-			return;
-		rawIn.flip();
-		SSLEngineResult r = sslEngine.unwrap(rawIn, app);
-		logger.debug("unwrap {}", r.getStatus());
-		rawIn.compact();
+		while (true) {
+			l = channel.read(rawIn);
+			if (l == -1) {
+				in.close();
+				return;
+			}
+			if (l == 0 && rawIn.position() == 0)
+				return;
+			rawIn.flip();
+			SSLEngineResult r = sslEngine.unwrap(rawIn, app);
+			logger.debug("unwrap {}", r.getStatus());
+			rawIn.compact();
 
-		app.flip();
-		pendingRead().write(app);
-		app.compact();
-		onRead();
+			app.flip();
+			pendingRead().write(app);
+			app.compact();
+			onRead();
+		}
 	}
 
 	@Override
